0aec776b9607170a73407caa5e956be850ce9f39
Laychiva Chhout <86119047+Laychiva@users.noreply.github.com>
2022-01-22 01:21:29 +0100

    Update contenu_contacts.php
    supprime inutil css package

--- content/contenu_contacts.php
+++ content/contenu_contacts.php
@@ -1,20 +1,17 @@
-<style>
-    .container {
-        width: 500px;
-        margin-left: auto;
-        margin-right: auto;
-    }
-</style>
 <div class="container pt-3">
     <form autocomplete="off">
         <fieldset>
-            <h2>Contact us</h2>
+            <div class="row text-center text-white">
+                <div class="col-lg-8 mx-auto">
+                    <h1 style='color:white'>Write us a message!</h1>
+                </div>
+            </div>
             <div class="form-group">
-                <label for="exampleInputEmail1" class="form-label mt-4">Email address</label>
+                <label for="exampleInputEmail1" style='color:aliceblue' class="form-label mt-4">Email address</label>
                 <input autocomplete="off" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
             </div>
             <div class="form-group">
-                <label for="sport_section" class="form-label mt-4">Where do you come from?</label>
+                <label for="sport_section" class="form-label mt-4" style='color:aliceblue'>Where do you come from?</label>
                 <select class="form-select" id="sport_section">
                     <option>Crossfit</option>
                     <option>Tennis</option>
@@ -26,11 +23,11 @@
                 </select>
             </div>
             <div class="form-group">
-                <label for="exampleTextarea" class="form-label mt-4">Your message:</label>
+                <label for="exampleTextarea" style='color:aliceblue' class="form-label mt-4">Your message:</label>
                 <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
             </div>
             <br>
-            <center><button type="submit" class="btn btn-primary">Submit</button> We will reply as soon as possible.</center>
+            <center style='color:aliceblue'><button type="submit" class="btn btn-primary">Submit</button> We will reply as soon as possible.</center>
             </br>
         </fieldset>
     </form>
